57cc94215caafcd82abb5f75b077f1209a7848ff
Denis Arh <denis.arh@gmail.com>
2020-11-20 07:27:10 +0100

    Remove MONOLITH_API param & logic

--- webapp/src/autoconfig.sh
+++ webapp/src/autoconfig.sh
@@ -14,10 +14,6 @@ BASEDIR=${BASEDIR:-"/webapp"}
 # if it does, it should hold the hostname of where these webapps are served
 VIRTUAL_HOST=${VIRTUAL_HOST:-"local.cortezaproject.org"}
 
-# If we're serving from one API (monolith) this should have non empty value
-# If not (microservices), then it should be empty
-MONOLITH_API=${MONOLITH_API:-""}
-
 # Assume API is using same scheme as webapps
 API_SCHEME=${API_SCHEME:-""}
 
@@ -25,17 +21,10 @@ API_SCHEME=${API_SCHEME:-""}
 API_PREFIX=${API_PREFIX:-"api."}
 
 # Loggic differs a bit when dealing with monolith vs. microservice setup
-if [ ! -z "${MONOLITH_API}" ]; then
-  API_BASEURL=${API_BASEURL:-"${API_PREFIX}${VIRTUAL_HOST}"}
-  API_BASEURL_SYSTEM=${API_BASEURL_SYSTEM:-"${API_SCHEME}//${API_BASEURL}/system"}
-  API_BASEURL_MESSAGING=${API_BASEURL_MESSAGING:-"${API_SCHEME}//${API_BASEURL}/messaging"}
-  API_BASEURL_COMPOSE=${API_BASEURL_COMPOSE:-"${API_SCHEME}//${API_BASEURL}/compose"}
-else
-  API_BASEURL=${API_BASEURL:-"${VIRTUAL_HOST}"}
-  API_BASEURL_SYSTEM=${API_BASEURL_SYSTEM:-"${API_SCHEME}//system.${API_PREFIX}${API_BASEURL}"}
-  API_BASEURL_MESSAGING=${API_BASEURL_MESSAGING:-"${API_SCHEME}//messaging.${API_PREFIX}${API_BASEURL}"}
-  API_BASEURL_COMPOSE=${API_BASEURL_COMPOSE:-"${API_SCHEME}//compose.${API_PREFIX}${API_BASEURL}"}
-fi
+API_BASEURL=${API_BASEURL:-"${API_PREFIX}${VIRTUAL_HOST}"}
+API_BASEURL_SYSTEM=${API_BASEURL_SYSTEM:-"${API_SCHEME}//${API_BASEURL}/system"}
+API_BASEURL_MESSAGING=${API_BASEURL_MESSAGING:-"${API_SCHEME}//${API_BASEURL}/messaging"}
+API_BASEURL_COMPOSE=${API_BASEURL_COMPOSE:-"${API_SCHEME}//${API_BASEURL}/compose"}
 
 
 tee \

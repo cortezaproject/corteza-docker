58fca0dfee4344456d562168c550d7cd33080271
Denis Arh <denis.arh@gmail.com>
2021-01-27 14:51:13 +0100

    Tweak healtchecks on server & webapp

--- server/src/healthcheck.sh
+++ server/src/healthcheck.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-curl --silent --fail-early http://127.0.0.1:${C_HTTP_PORT}/healthcheck || exit 1
+curl --silent --fail --fail-early http://127.0.0.1:${C_HTTP_PORT}/healthcheck || exit 1
